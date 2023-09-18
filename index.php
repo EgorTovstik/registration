73efb63acf3f4ae6e4b278e8a106d621775bbbd5
EgorTovstik <eg1707or2003@gmail.com>
2023-09-19 01:25:02 +0700

    Y

--- index.php
+++ index.php
@@ -2,23 +2,35 @@
 
 <html lang = "en">
 <head>
-
     <meta charset="UTF-8">
     <title>Авторизация и регистрация</title>
     <link rel="stylesheet" href="/assets/main.css"
 </head>
 <body>
-
     <!--Форма авторизации-->
-    <form>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') { ?>
+    <form action="index.php" method="post">
         <label>Логин</label>
-        <input type="text" placeholder="Введите свой логн">
+        <input type="text" placeholder="Введите свой логн" value="<?php echo $_POST['login'] ?? '' ?>">
         <label>Пароль</label>
-        <input type="password" placeholder="Введите свой пароль">
-        <button>Войти</button>
+        <input type="password" placeholder="Введите свой пароль" value="<?php echo $_POST['pswrd'] ?? '' ?>">
+        <div class="mb-3">
+            <input type="submit" class="btn btn-primary" value="Войти">
+        </div>
         <p>
             У вас нет аккаунта? - <a href="/registr.php">зарегистрируйтесь!</a>
         </p>
     </form>
+        <?php
+    }
+    ?>
+    <div class="container">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            echo '<b>login</b> = ' . ($_POST['login'] ?? '') . '<br>';
+            echo 'pswrd = ' . ($_POST['pswrd'] ?? '');
+        }
+        ?>
+    </div>
 </body>
 </html>
--- registr.php
+++ registr.php
@@ -9,23 +9,37 @@
 </head>
 <body>
 
-    <!--Форма авторизации-->
-    <form>
+    <!--Форма Регистрации-->
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') { ?>
+    <form action="index.php" method="post">
         <label>ФИО</label>
-        <input type="text" placeholder="Введите свое ФИО">
+        <input type="text" placeholder="Введите свое ФИО" value="<?php echo $_POST['FIO'] ?? '' ?>">
         <label>Почта</label>
-        <input type="email" placeholder="Введите адрес электронной почты">
+        <input type="email" placeholder="Введите адрес электронной почты" value="<?php echo $_POST['email'] ?? '' ?>">
         <label>Логин</label>
-        <input type="text">
+        <input type="text" value="<?php echo $_POST['login'] ?? '' ?>">
         <label>Пароль</label>
-        <input type="password" placeholder="Введите свой пароль">
+        <input type="password" placeholder="Введите свой пароль" value="<?php echo $_POST['pswrd'] ?? '' ?>">
         <label>Подтверждение пароля</label>
-        <input type="password" placeholder="Подтвердите пароль">
+        <input type="password" placeholder="Подтвердите пароль" value="<?php echo $_POST['pswrd'] ?? '' ?>">
         <button>Войти</button>
         <p>
             У вас уже есть аккаунт? - <a href="/index.php">авторизируйтесь!</a>
         </p>
     </form>
+        <?php
+    }
+    ?>
+    <div class="container">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            echo 'FIO = ' . ($_POST['FIO'] ?? '');
+            echo 'email = ' . ($_POST['email'] ?? '');
+            echo '<b>login</b> = ' . ($_POST['login'] ?? '') . '<br>';
+            echo 'pswrd = ' . ($_POST['pswrd'] ?? '');
+        }
+        ?>
+    </div>
 </body>
 </html>
 
