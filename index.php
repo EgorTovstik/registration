d86dab5fc1d24e280231493dba8163084d6a3f2f
EgorTovstik <eg1707or2003@gmail.com>
2023-09-19 00:14:38 +0700

    Y

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang = "en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="/assets/main.css"
+</head>
+<body>
+
+    <!--Форма авторизации-->
+    <form action="" method="">
+        <label>Логин</label>
+        <input type="text" placeholder="Введите свой логн">
+        <label>Пароль</label>
+        <input type="password" placeholder="Введите свой пароль">
+        <button>Войти</button>
+        <p>
+            У вас нет аккаунта? - <a href="/registr.php">зарегистрируйтесь!</a>
+        </p>
+    </form>
+</body>
+</html>
---
+++ registr.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang = "en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="/assets/main.css"
+</head>
+<body>
+
+    <!--Форма авторизации-->
+    <form action="" method="">
+        <label>ФИО</label>
+        <input type="text" placeholder="Введите свое ФИО">
+        <label>Почта</label>
+        <input type="email" placeholder="Введите адрес электронной почты">
+        <label>Логин</label>
+        <input type="text">
+        <label>Пароль</label>
+        <input type="password" placeholder="Введите свой пароль">
+        <label>Подтверждение пароля</label>
+        <input type="password" placeholder="Подтвердите пароль">
+        <button>Войти</button>
+        <p>
+            У вас уже есть аккаунт? - <a href="/index.php">авторизируйтесь!</a>
+        </p>
+    </form>
+</body>
+</html>
+
